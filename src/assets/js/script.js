e55dcd0c01d2bcdcb546d60df1db12476683f6d3
alex <maxko111@yandex.ru>
2023-07-14 15:47:34 +0300

    add timer script

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -154,6 +154,58 @@ window.addEventListener('DOMContentLoaded', () => {
 		};
 	});
 	// end form
+
+	// Timer
+	let deadLine = [2023, 7, 27, 0];
+
+function getTimeRemaining(endtime) {
+	let t = Date.parse(new Date(...endtime)) - Date.parse(new Date);
+	let seconds = Math.floor((t / 1000) % 60);
+	let minutes = Math.floor((t / 1000 / 60) % 60);
+	let hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+	let days = Math.floor((t / 1000 / 60 / 60 / 24));
+	return {
+		"total": t,
+		"days": days,
+		"hours": hours,
+		"minutes": minutes,
+		"seconds": seconds,
+	}
+};
+
+setClock("timer", deadLine);
+function setClock(id, endtime) {
+	let timer = document.getElementById(id);
+	let days = timer.querySelector("#days");
+	let hours = timer.querySelector("#hours");
+	let minutes = timer.querySelector("#minutes");
+	let seconds = timer.querySelector("#seconds");
+	let timeInterval = setInterval(updateClock, 1000);
+
+	function updateClock() {
+		let t = getTimeRemaining(endtime);
+		if (t.total <= 0) {
+			// clearInterval(timeInterval);
+			// days.textContent = "00";
+			// hours.textContent = "00";
+			// minutes.textContent = "00";
+			// seconds.textContent = "00";
+			deadLine[1]++;
+			updateClock();
+
+		} else {
+			if (t.days < 10) days.textContent = "0" + t.days;
+			else days.textContent = t.days;
+			if (t.hours < 10) hours.textContent = "0" + t.hours;
+			else hours.textContent = t.hours;
+			if (t.minutes < 10) minutes.textContent = "0" + t.minutes;
+			else minutes.textContent = t.minutes;
+			if (t.seconds < 10) seconds.textContent = "0" + t.seconds;
+			else seconds.textContent = t.seconds;
+		}
+	}
+}
+// end timer
 	let descr = document.querySelector("meta[name='description']");
 	console.log(descr.content.length)
 
